e6da19c536e7d7a3aa0f8fd8977ae3dbbeba22d0
Jeff Chang <molloy@gmail.com>
2013-03-01 15:12:12 -0800

    initial commit

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("#menu ul").superfish({
+		autoArrows:  false
+	});
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccu::Application.config.secret_token = 'f959e4ff9b36706934519dc03e4fc39aef6db06f73b54b9f42c3287750adef1dc95e36a69bddd726ed50683595273ab11cb54a7dbdb17147ccbef0991f200ce2'
