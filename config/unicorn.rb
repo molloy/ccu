7ff52db0437487b678a5a8fbde1155640988e932
Jeff Chang <molloy@gmail.com>
2013-03-24 13:57:02 -0700

    corrected unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,8 +4,9 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
     rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
     rvm_lib_path = File.join(rvm_path, 'lib')
+    # $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
-    RVM.use_from_path! APP_ROOT
+    # RVM.use_from_path! APP_ROOT
   rescue LoadError
     raise "RVM ruby lib is currently unavailable."
   end
