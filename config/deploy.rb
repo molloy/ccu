d38a5fed99e4c2483a1f6810342e2280462e3e87
Jeff Chang <molloy@gmail.com>
2013-03-24 23:41:50 -0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -116,9 +116,9 @@ namespace :deploy do
         from = source.next_revision(current_revision)
         # if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        # else
-          # logger.info "Skipping asset pre-compilation because there were no asset changes"
-        # end
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
       else
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       end
