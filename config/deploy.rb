4a691d8f5e996962f35c7d68ffb18366bbedab02
Jeff Chang <molloy@gmail.com>
2013-03-24 21:57:30 -0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :user, "ec2-user"
 server "ec2-184-73-13-170.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/jinny2.pem"]
 
-load "deploy/assets"
+load 'deploy/assets'
 # set :stages, %w(production staging)
 # set :default_stage, "production"
 # require 'capistrano/ext/multistage'
