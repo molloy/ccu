5e6cf68e4ffd2de59cacfe9de7701c5cc77d3595
Jeff Chang <molloy@gmail.com>
2013-03-25 01:25:13 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Ccu::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+  config.assets.initialize_on_precompile = true
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -47,6 +49,7 @@ Ccu::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( refinery/refinery.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
