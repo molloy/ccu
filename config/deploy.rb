0004da4b2d2967174636f95f22e11b2650a56d12
Jeff Chang <molloy@gmail.com>
2013-03-25 00:00:15 -0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -114,7 +114,7 @@ namespace :deploy do
       # If this is our first deploy - don't check for the previous version
       if remote_file_exists?(current_path)
         from = source.next_revision(current_revision)
-        # if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
         else
           logger.info "Skipping asset pre-compilation because there were no asset changes"
