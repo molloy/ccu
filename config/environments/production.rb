0dc6e987c65df3457e31de8935df67c9ae4ee6a2
Jeff Chang <molloy@gmail.com>
2013-03-29 13:37:37 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Ccu::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( refinery/refinery.css *.png *.jpg *.jpeg *.gif )
+  config.assets.precompile += %w( refinery/refinery.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
