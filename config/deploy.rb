7bf7fc1fe064625ecb44971305c3bd5d9c564698
Jeff Chang <molloy@gmail.com>
2013-09-01 22:01:26 -0700

    changed deploy targets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 #production.rb
 set :user, "ec2-user"
-server "ec2-54-243-17-132.compute-1.amazonaws.com", :app, :web, :db, :primary => true
-ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/jinny2.pem"]
+server "ec2-54-215-134-159.us-west-1.compute.amazonaws.com", :app, :web, :db, :primary => true
+ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/jinny.pem"]
 
 load 'deploy/assets'
 set :bundle_flags,    ""
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 #production.rb
 set :user, "ec2-user"
-server "ec2-184-73-13-170.compute-1.amazonaws.com", :app, :web, :db, :primary => true
-ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/jinny2.pem"]
+server "ec2-54-215-134-159.us-west-1.compute.amazonaws.com", :app, :web, :db, :primary => true
+ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/jinny.pem"]
