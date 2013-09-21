5e3dc968c614f518652cd95ab030b45fd9fc8a72
Jeff Chang <molloy@gmail.com>
2013-09-20 22:37:21 -0700

    cap asset precompile is now moved to local

--- config/deploy.rb
+++ config/deploy.rb
@@ -112,15 +112,32 @@ namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       # If this is our first deploy - don't check for the previous version
-      if remote_file_exists?(current_path)
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        else
-          logger.info "Skipping asset pre-compilation because there were no asset changes"
-        end
+
+      # if remote_file_exists?(current_path)
+      #   from = source.next_revision(current_revision)
+      #   if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      #     run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      #   else
+      #     logger.info "Skipping asset pre-compilation because there were no asset changes"
+      #   end
+      # else
+      #   run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      # end
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+
+        run ("rm -rf #{latest_release}/public/assets &&
+              mkdir -p #{latest_release}/public &&
+              mkdir -p #{shared_path}/assets &&
+              ln -s #{shared_path}/assets #{latest_release}/public/assets")
       else
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        logger.info "Skipping asset precompilation because there were no asset changes"
       end
     end
   end
